faaba5dc3d3fd0ab49bcd1b2d5014c315c2db821
Lucky <luckyknguyen@gmail.com>
2017-02-25 18:30:17 -0800

    styled menu

--- app/js/script.js
+++ app/js/script.js
@@ -4,47 +4,48 @@ $(document).ready(function() {
   );
 
   var products=[
-    {name: "KushCo OG", type: "indica" },
-    {name: "Skywalker OG", type: "indica"  },
-    {name: "Hardcore OG", type: "indica" },
-    {name: "Pure OG", type: "indica" },
-    {name: "Pineapple Express", type: "sativa" },
-    {name: "Baby Blue", type: "sativa"},
-    {name: "Banana Split", type: "sativa"},
-    {name: "Strawberry Banana", type: "sativa"},
-    {name: "Supreme Blue Dream", type: "sativa"},
-    {name: "Thin Mint", type: "Hybrid"},
-    {name: "Forum Cookies", type: "Hybrid"},
-    {name: "Purple Kryptonite", type: "Hybrid"},
-    {name: "Shatter Dogo", type:"concentrate"},
-    {name: "Skywalker OG Shatter", type:"concentrate"},
-    {name: "Green Crack Shatter", type:"concentrate"},
-    {name: "Animal Spirits - Holy Grail", type:"concentrate"},
-    {name: "Animal Spirits - Girl Scout Cookies", type:"concentrate"},
-    {name: "Blue Dream Shatter", type:"concentrate"},
-    {name: "Brass Knuckles - Jack Herer", type:"concentrate"},
-    {name: "Brass Knuckles - Gorilla Glue", type:"concentrate"},
-    {name: "Brass Knuckles - Girl Scout Cookies", type:"concentrate"},
-    {name: "Brass Knuckles - Raw", type:"concentrate"},
-    {name: "Brass Knuckles - Blue Dream", type:"concentrate"},
-    {name: "Brass Knuckles - Abracadabra", type:"concentrate"},
-    {name: "Brass Knuckles - Blueberry", type:"concentrate"},
-    {name: "Brass Knuckles - Grape Ape", type:"concentrate"},
-    {name: "Brass Knuckles - Purple Haze", type:"concentrate"},
-    {name: "Brass Knuckles - SFV OG", type:"concentrate"},
-    {name: "Brass Knuckles - Skywalker OG", type:"concentrate"},
-    {name: "Brass Knuckles - Strawberry Haze", type:"concentrate"},
-    {name: "Brass Knuckles - Candy Apple", type:"concentrate"},
-    {name: "Kiva - Ginger Dark Chocolate", type:"edible"},
-    {name: "Kiva Bar - 4 Flavors", type:"edible"},
-    {name: "Kiva - Expresso Dark Chocolate", type:"edible"},
-    {name: "Kiva - Terra Bites - Expresso Beans", type:"edible"},
-    {name: "Kiva - Terra Bites - Blueberries", type:"edible"},
-    {name: "Kiva Bar", type:"edible"},
-    {name: "HealthCare Bar - Vegan", type:"edible"},
-    {name: "Brass Knuckes - Adjustable Voltage Gold Battery", type:"gear"},
-    {name: "Brass Knuckes - Gold Vape Battery", type:"gear"},
-    {name: "Brass Knuckes - Woodgrain Battery", type:"gear"}
+    {name: "KushCo OG", type: "indica", eighth: "1/8", fourth: "1/4", half: "1/2", oz: "1 oz", eighthprice: "$55.00", fourthprice: "$110.00", halfprice: "--", ozprice: "--"},
+    {name: "Skywalker OG", type: "indica" , eighth: "1/8", fourth: "1/4", half: "1/2", oz: "1 oz", eighthprice: "$45.00", fourthprice: "$90.00", halfprice: "--", ozprice: "--"},
+    {name: "Holy Grail OG", type: "indica", eighth: "1/8", fourth: "1/4", half: "1/2", oz: "1 oz",  eighthprice: "$40.00", fourthprice: "$80.00", halfprice: "--", ozprice: "--"},
+    {name: "Pure OG", type: "indica", eighth: "1/8", fourth: "1/4", half: "1/2", oz: "1 oz",  eighthprice: "$35.00", fourthprice: "$65.00", halfprice: "$120.00", ozprice: "$200.00"},
+    {name: "Pineapple Express", type: "sativa", eighth: "1/8", fourth: "1/4", half: "1/2", oz: "1 oz",  eighthprice: "$45.00", fourthprice: "$90.00", halfprice: "$160.00", ozprice: "--"},
+    {name: "Baby Blue", type: "sativa", eighth: "1/8", fourth: "1/4", half: "1/2", oz: "1 oz",  eighthprice: "$45.00", fourthprice: "$90.00", halfprice: "$160.00", ozprice: "$260.00"},
+    {name: "Banana Split", type: "sativa", eighth: "1/8", fourth: "1/4", half: "1/2", oz: "1 oz",  eighthprice: "$45.00", fourthprice: "$90.00", halfprice: "--", ozprice: "--"},
+    {name: "Strawberry Banana", type: "sativa", eighth: "1/8", fourth: "1/4", half: "1/2", oz: "1 oz",  eighthprice: "$40.00", fourthprice: "$80.00", halfprice: "$150.00", ozprice: "--"},
+    {name: "Supreme Blue Dream", type: "sativa", eighth: "1/8", fourth: "1/4", half: "1/2", oz: "1 oz",  eighthprice: "$40.00", fourthprice: "$80.00", halfprice: "$140.00", ozprice: "$260.00"},
+    {name: "Thin Mint", type: "Hybrid", eighth: "1/8", fourth: "1/4", half: "1/2", oz: "1 oz",  eighthprice: "$40.00", fourthprice: "$80.00", halfprice: "$140.00", ozprice: "--"},
+    {name: "Forum Cookies", type: "Hybrid", eighth: "1/8", fourth: "1/4", half: "1/2", oz: "1 oz",  eighthprice: "$40.00", fourthprice: "$80.00", halfprice: "$140.00", ozprice: "--"},
+    {name: "Purple Kryptonite", type: "Hybrid", eighth: "1/8", fourth: "1/4", half: "1/2", oz: "1 oz",  eighthprice: "$40.00", fourthprice: "$80.00", halfprice: "$150.00", ozprice: "$260.00"},
+    {name: "Nug Run - White Walker (Shatter)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "$26.00", oneprice: "$48.00", twoprice: "$92.00"},
+    {name: "Skywalker OG Shatter", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "$25.00", oneprice: "$45.00", twoprice: "$85.00"},
+    {name: "Green Crack Shatter", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "$25.00", oneprice: "$45.00", twoprice: "$85.00"},
+    {name: "Animal Spirits - Holy Grail OG Vape Cartridge", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "$25.00", oneprice: "$40.00", twoprice: "$92.00"},
+    {name: "Animal Spirits - Girl Scout Cookies Vape Cartridge", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "$25.00", oneprice: "$40.00", twoprice: "--"},
+    {name: "Animal Spirits - Blue Dream Vape Cartridge", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "$25.00", oneprice: "$40.00", twoprice: "$80.00"},
+    {name: "Blue Dream Shatter", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "$25.00", oneprice: "$45.00", twoprice: "$85.00"},
+    {name: "Brass Knuckles - Jack Herer (Sativa)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "--", oneprice: "$60.00", twoprice: "--"},
+    {name: "Brass Knuckles - Strawberry Cough (Sativa)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "--", oneprice: "$60.00", twoprice: "--"},
+    {name: "Brass Knuckles - Candy Apple (Hybrid)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "--", oneprice: "$60.00", twoprice: "--"},
+    {name: "Brass Knuckles - Blue Dream (Sativa)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "--", oneprice: "$60.00", twoprice: "--"},
+    {name: "Brass Knuckles - Gorilla Glue (Hybrid)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "--", oneprice: "$60.00", twoprice: "--"},
+    {name: "Brass Knuckles - Girl Scout Cookies (Hybrid)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "--", oneprice: "$60.00", twoprice: "--"},
+    {name: "Brass Knuckles - Raw (Hybrid)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "--", oneprice: "$60.00", twoprice: "--"},
+    {name: "Brass Knuckles - Abracadabra (Hybrid)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "--", oneprice: "$60.00", twoprice: "--"},
+    {name: "Brass Knuckles - Blueberry (Indica)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "--", oneprice: "$60.00", twoprice: "--"},
+    {name: "Brass Knuckles - Grape Ape (Indica)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "--", oneprice: "$60.00", twoprice: "--"},
+    {name: "Brass Knuckles - Purple Haze (Sativa)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "--", oneprice: "$60.00", twoprice: "--"},
+    {name: "Brass Knuckles - SFV OG (Indica)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "--", oneprice: "$60.00", twoprice: "--"},
+    {name: "Brass Knuckles - Skywalker OG (Indica)", type:"concentrate", half: "0.5 g", one: "1 g", two: "2 g", halfprice: "--", oneprice: "$60.00", twoprice: "--"},
+    {name: "Kiva - Ginger Dark Chocolate", type:"edible", price: "$32.00"},
+    {name: "Kiva Bar - 4 Flavors", type:"edible", price: "$20.00"},
+    {name: "Kiva - Expresso Dark Chocolate", type:"edible", price: "$20.00"},
+    {name: "Kiva - Terra Bites - Expresso Beans", type:"edible", price: "$18.00"},
+    {name: "Kiva - Terra Bites - Blueberries", type:"edible", price: "$18.00"},
+    {name: "Kiva Bar", type:"edible", price: "$18.00"},
+    {name: "HealthCare Bar - Vegan", type:"edible", price: "$10.00"},
+    {name: "Brass Knuckes - Adjustable Voltage Gold Battery", type:"gear", price: "$30.00"},
+    {name: "Brass Knuckes - Gold Vape Battery", type:"gear", price: "$25.00"},
+    {name: "Brass Knuckes - Woodgrain Battery", type:"gear", price: "$25.00"}
   ];
 
   var template = $('#menu').html();
@@ -53,5 +54,14 @@ $(document).ready(function() {
   var html = templateScript(content);
 
   $(document.body).append(html);
+
+  // Remove blank labeled buttons 
+  var quantityButton = document.querySelectorAll(".quantity");
+  console.log(quantityButton);
+  for(i = 0; i < quantityButton.length; i++){
+    if(quantityButton[i].textContent == ''){
+      quantityButton[i].style.display = 'none';
+    }
+  }
 
 });
