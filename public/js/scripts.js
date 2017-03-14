c89eb1dc1fb00350dc078a95546884b4827d9880
Jacob Terrado <jaketerrado@gmail.com>
2017-03-13 20:36:44 -0700

    back to -150

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -775,31 +775,31 @@ $(document).ready(function () {
 
     $("#indica-label").click(function () {
         $('html, body').animate({
-            scrollTop: $("#indica").offset().top - 120
+            scrollTop: $("#indica").offset().top - 150
         }, 500);
     });
 
     $("#sativa-label").click(function () {
         $('html, body').animate({
-            scrollTop: $("#sativa").offset().top - 120
+            scrollTop: $("#sativa").offset().top - 150
         }, 500);
     });
 
     $("#hybrid-label").click(function () {
         $('html, body').animate({
-            scrollTop: $("#hybrid").offset().top - 120
+            scrollTop: $("#hybrid").offset().top - 150
         }, 500);
     });
 
     $("#concentrate-label").click(function () {
         $('html, body').animate({
-            scrollTop: $("#concentrate").offset().top - 120
+            scrollTop: $("#concentrate").offset().top - 150
         }, 500);
     });
 
     $("#edible-label").click(function () {
         $('html, body').animate({
-            scrollTop: $("#edible").offset().top - 120
+            scrollTop: $("#edible").offset().top - 150
         }, 500);
     });
 
